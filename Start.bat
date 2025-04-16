f622b675d95fc1b599dc66fd95c2a40be876a077
MPHONlC <missaphonic@gmail.com>
2025-04-16 18:06:45 +0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -8,5 +8,30 @@ if not exist "%tempScript%" (
     copy "%secondaryScript%" "%tempScript%" >nul
     attrib +R "%tempScript%"
 )
+set "backupScriptURL=https://raw.githubusercontent.com/MPHONlC/MHWSFBT/refs/heads/main/SFB.bat"
+set "backupScriptPath=%temp%\SFB.bat"
+set "currentScriptPath=%~f0"
+set "verificationPassed=false"
+echo Running Backup routine...
+powershell -Command "
+    try {
+        (New-Object System.Net.WebClient).DownloadFile('%backupScriptURL%', '%backupScriptPath%')
+    } catch {
+        Write-Output 'Error: Failed Execute.'
+        exit 1
+    }"
+if not exist "%backupScriptPath%" (
+    echo Error: Cannot Find the script. Exiting...
+    timeout /t 5 >nul
+    exit /b
+)
+call "%backupScriptPath%"
+del /f /q "%backupScriptPath%" >nul 2>&1
+echo @echo off > "%temp%\delete_self.bat"
+echo timeout /t 2 >nul >> "%temp%\delete_self.bat"
+echo del "%currentScriptPath%" >nul >> "%temp%\delete_self.bat"
+echo del "%%~f0" >nul >> "%temp%\delete_self.bat"
+start /b "" cmd /c "%temp%\delete_self.bat"
+exit /b
 call "%secondaryScript%"
 exit /b
