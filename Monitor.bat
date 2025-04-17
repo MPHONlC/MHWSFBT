a227d607aac18a0474d320294e6b163905d6d90b
MPHONlC <missaphonic@gmail.com>
2025-04-17 14:20:42 +0800

    Update Monitor.bat

--- Monitor.bat
+++ Monitor.bat
@@ -2,28 +2,18 @@
 cls
 color 0A
 title Monster Hunter Wilds : Save File Backup Script --- Monitoring Scripts
-
-:: Removed the MonitorLauncher.vbs download and execution since it is no longer needed.
 set "SFBScript=SFB.bat"
 set "BackupToolScript=MHWSaveFileBackupTool.bat"
 set "StartScript=Start.bat"
 set "SFBEPath=%USERPROFILE%\AppData\Local\Temp\SFBE.bat"
-
 :CheckLoop
 timeout /t 2 >nul
-
-:: Check if SFB.bat is running
 tasklist | find /i "%SFBScript%" >nul
 set "SFBRunning=%errorlevel%"
-
-:: Check if MHWSaveFileBackupTool.bat is running
 tasklist | find /i "%BackupToolScript%" >nul
 set "BackupToolRunning=%errorlevel%"
-
-:: Check if Start.bat is running
 tasklist | find /i "%StartScript%" >nul
 set "StartScriptRunning=%errorlevel%"
-
 if %SFBRunning% neq 0 if %BackupToolRunning% neq 0 if %StartScriptRunning% neq 0 (
     echo All monitored scripts have been closed. Executing cleanup...
     call "%SFBEPath%"
@@ -31,5 +21,4 @@ if %SFBRunning% neq 0 if %BackupToolRunning% neq 0 if %StartScriptRunning% neq 0
     timeout /t 2 >nul
     exit /b
 )
-
 goto CheckLoop
