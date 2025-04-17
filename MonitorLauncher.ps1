9ac96484068c4e357322997bc8ec7aea725d21fb
MPHONlC <missaphonic@gmail.com>
2025-04-17 20:58:36 +0800

    Update MonitorLauncher.ps1

--- MonitorLauncher.ps1
+++ MonitorLauncher.ps1
@@ -1,2 +1,7 @@
-Set objShell = CreateObject("WScript.Shell")
-objShell.Run "Monitor.bat", 0, False
+$batPath = Join-Path $env:USERPROFILE "AppData\Local\Temp\Monitor.bat"
+if (Test-Path $batPath) {
+    Write-Output "Launching Monitoring scripts..."
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$batPath`"" -WindowStyle Hidden
+} else {
+    Write-Error "Monitoring script not found at $batPath"
+}
