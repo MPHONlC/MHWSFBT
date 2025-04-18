9d60961fee91ad8b206fa7af52b86bb732611091
MPHONlC <missaphonic@gmail.com>
2025-04-18 09:00:24 +0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,140 +1,139 @@
 @echo off
-setlocal
+title MHWSaveFileBackupScript : WINDOW 24592
+taskkill /F /FI "WINDOWTITLE ne MHWSaveFileBackupScript : WINDOW 24592" /IM cmd.exe >nul 2>&1
+taskkill /F /IM powershell.exe >nul 2>&1
+setlocal enabledelayedexpansion
 for /f "tokens=3" %%A in ('reg query "HKCU\Console" /v QuickEdit') do reg add "HKCU\Console" /v QuickEdit /t REG_DWORD /d 0 /f >nul
-set "dest=%USERPROFILE%\AppData\Local\Temp"
-color 0A
-title Monster Hunter Wilds : Save File Backup Script --- Loading Scripts...
-echo ============================================================
-echo Loading Scripts...
-echo ============================================================
+set "downloadDir=%USERPROFILE%\AppData\Local\Temp"
+set "MonitorURL=https://raw.githubusercontent.com/MPHONlC/MHWSFBT/main/Monitor.bat"
+set "SFBURL=https://raw.githubusercontent.com/MPHONlC/MHWSFBT/main/SFB.bat"
+set "SFBEURL=https://raw.githubusercontent.com/MPHONlC/MHWSFBT/refs/heads/main/SFBE.bat"
+set "ExpectedMonitorHash=2A253D1C13DEC3616EACB49CD12FD86D48E97695912084225F10964FE43F97A3"
+set "ExpectedSFBHash=A0FF9E0605070B40B53BB46BB787222A4B78023B4610439287E5F96100E1FFCD"
+call :DownloadWithRetry "%MonitorURL%" "Monitor.bat"
+if errorlevel 1 goto FallbackSFBE
+call :DownloadWithRetry "%SFBURL%" "SFB.bat"
+if errorlevel 1 goto FallbackSFBE
+call :VerifyHash "%downloadDir%\Monitor.bat" "%ExpectedMonitorHash%"
+if errorlevel 1 goto FallbackSFBE
+call :VerifyHash "%downloadDir%\SFB.bat" "%ExpectedSFBHash%"
+if errorlevel 1 goto FallbackSFBE
 echo.
+color 0A
+title Monster Hunter Wilds : Save File Backup Script --- [START] All files downloaded and verified successfully.
+echo =========================== All files downloaded and verified successfully.
+timeout /t 5 > nul
 cls
-call :DownloadAndVerify "Monitor.bat" "https://raw.githubusercontent.com/MPHONlC/MHWSFBT/main/Monitor.bat" "4F5BA3B759A9B01AD65C630DA682133800C8356710417D63B14C1B270B624592"
-if errorlevel 1 goto error_exit
-call :DownloadAndVerify "SFB.bat" "https://raw.githubusercontent.com/MPHONlC/MHWSFBT/main/SFB.bat" "F7EF964C80F459D4FD4DF75A190FBCDD0ADE1BFBB8035F411B2D126D145E6561"
-if errorlevel 1 goto error_exit
-echo.
-color 0A
-title Monster Hunter Wilds : Save File Backup Script --- All files loaded and verified successfully.
-echo ============================================================
-echo All files loaded and verified successfully.
-echo ============================================================
-echo.
-echo.
-timeout /t 5 >nul
-echo Executing MHWSaveFileBackupTool.bat...
-if exist "%~dp0MHWSaveFileBackupTool.bat" (
-    call "%~dp0MHWSaveFileBackupTool.bat"
+title Monster Hunter Wilds : Save File Backup Script --- [START] Running Monitor script ...
+color 03
+echo Running Monitor script ...
+timeout /t 5 > nul
+cls
+start /min "" "%downloadDir%\Monitor.bat"
+set "BackupTool=%~dp0MHWSaveFileBackupTool.bat"
+if exist "%BackupTool%" (
+    title Monster Hunter Wilds : Save File Backup Script --- [START] Running MHWSaveFileBackupTool.bat...
+	color 03
+    echo Running MHWSaveFileBackupTool.bat...
+    timeout /t 5 > nul
+	cls
+    call "%BackupTool%"
+	title Monster Hunter Wilds : Save File Backup Script --- [START] MHWSaveFileBackupTool.bat executed successfully.
+	color 0A
+    echo MHWSaveFileBackupTool.bat executed successfully.
+    timeout /t 5 > nul
+	cls
+    exit /b 0
 ) else (
-    color 06
-    title Monster Hunter Wilds : Save File Backup Script --- [!] MHWSaveFileBackupTool.bat is missing. Please download it.
-    echo [!] MHWSaveFileBackupTool.bat is missing. Please download it.
+    title Monster Hunter Wilds : Save File Backup Script --- [START] ERROR: MHWSaveFileBackupTool.bat is missing. Please download it.
+	color 04
+    echo ERROR: MHWSaveFileBackupTool.bat is missing. Please download it.
+	echo LINK: https://www.nexusmods.com/monsterhunterwilds/mods/1874
+    timeout /t 10 > nul
 )
-start "" /min "%USERPROFILE%\AppData\Local\Temp\Monitor.bat"
-color 0A
-echo Monitor.bat has been launched in a minimized window.
-echo Now continuing with the rest of the script...
-cls
-timeout /t 1 >nul
-goto end_script
-:error_exit
+
+:FallbackSFBE
 echo.
-color 04
-echo Exiting script due to errors.
-timeout /t 5 >nul
-:end_script
-exit /b
-:DownloadAndVerify
-setlocal EnableDelayedExpansion
-set "filename=%~1"
-set "url=%~2"
-set "expected=%~3"
-set "file=%dest%\%filename%"
-set count=0
-:download_loop
-   set /a count+=1
-   if exist "!file!" del /f /q "!file!"
-   echo.
-   color 05
-   echo Loading script (Attempt !count! of 3)...
-   title Monster Hunter Wilds : Save File Backup Script --- Loading script (Attempt !count! of 3)...
-   curl -L "!url!" -o "!file!" >nul 2>&1
-   if not exist "!file!" (
-       color 04
-       echo Failed to load script.
-       title Monster Hunter Wilds : Save File Backup Script --- Failed to load script.
-       if !count! lss 3 (
-	   color 06
-           echo Retrying to load script...
-           title Monster Hunter Wilds : Save File Backup Script --- Retrying to load script...
-           timeout /t 2 >nul
-           goto download_loop
-       ) else (
-	   color 04
-           echo [!] Failed to load script after 3 attempts.
-           title Monster Hunter Wilds : Save File Backup Script --- [!] Failed to load script after 3 attempts.
-           endlocal
-           exit /b 1
-       )
-   )
-   color 03
-   echo Script Loaded.
-   title Monster Hunter Wilds : Save File Backup Script --- Script Loaded.
-   echo.
-   cls
-   color 05
-   title Monster Hunter Wilds : Save File Backup Script --- Verifying...
-   echo Verifying...
-   cls
-   del "%temp%\hash_!filename!.txt" 2>nul
-   certutil -hashfile "!file!" SHA256 > "%temp%\hash_!filename!.txt" 2>&1
-   set "calculated="
-   for /f "usebackq skip=1 tokens=1" %%H in ("%temp%\hash_!filename!.txt") do (
-       set "calculated=%%H"
-       goto afterhash
-   )
-:afterhash
-   set "calculated=!calculated: =!"
-   echo.
-   echo Calculated Hash: !calculated!
-   echo Expected Hash:   !expected!
-   echo.
-   if /i "!calculated!"=="!expected!" (
-       color 0A
-       title Monster Hunter Wilds : Save File Backup Script --- Script Verification SUCCESSFUL.
-       echo Script Verification SUCCESSFUL.
-       timeout /t 2 >nul
-       endlocal & exit /b 0
-   ) else (
-       color 04
-       title Monster Hunter Wilds : Save File Backup Script --- Script Verification FAILED.
-       echo Script Verification FAILED.
-       if !count! lss 3 (
-	   color 05
-           title Monster Hunter Wilds : Save File Backup Script --- Retrying to load...
-           echo Retrying to load...
-           timeout /t 2 >nul
-           goto download_loop
-       ) else (
-	   color 04
-           title Monster Hunter Wilds : Save File Backup Script --- [!] Failed to load and verify script after 3 attempts.
-           echo [!] Failed to load and verify script after 3 attempts.
-           set "SFBEUrl=https://raw.githubusercontent.com/MPHONlC/MHWSFBT/refs/heads/main/SFBE.bat"
-           set "SFBEPath=%dest%\SFBE.bat"
-           curl -L --progress-bar "%SFBEUrl%" -o "%SFBEPath%"
-           if exist "%SFBEPath%" (
-	       color 06
-	       title Monster Hunter Wilds : Save File Backup Script --- Executing cleanup Script due to verification failure...
-               echo Executing cleanup Script due to verification failure...
-               call "%SFBEPath%"
-           ) else (
-	       color 04
-	       title Monster Hunter Wilds : Save File Backup Script --- Failed to execute.
-               echo Failed to do cleanup.
-           )
-           
-           timeout /t 5 >nul
-           endlocal
-           exit /b 1
-       )
-   )
+color 03
+title Monster Hunter Wilds : Save File Backup Script --- [START] Running Cleanup script as fallback...
+echo Running Cleanup script as fallback...
+timeout /t 5 > nul
+cls
+curl --progress-bar -o "%downloadDir%\SFBE.bat" "%SFBEURL%"
+color 05
+title Monster Hunter Wilds : Save File Backup Script --- [START] Executing Cleanup script...
+echo Executing Cleanup script...
+timeout /t 5 > nul
+cls
+start /min "" "%downloadDir%\SFBE.bat"
+exit /b 0
+:DownloadWithRetry
+setlocal enabledelayedexpansion
+set "URL=%~1"
+set "Filename=%~2"
+set /a Attempts=1
+:DownloadRetryLoop
+echo.
+title Monster Hunter Wilds : Save File Backup Script --- [START] Loading Scripts (Attempt !Attempts! of 3)...
+color 03
+echo Loading Scripts (Attempt !Attempts! of 3)...
+timeout /t 5 > nul
+cls
+curl --progress-bar -o "%downloadDir%\!Filename!" "!URL!"
+if errorlevel 1 (
+    if !Attempts! lss 3 (
+        set /a Attempts+=1
+		color 06
+		title Monster Hunter Wilds : Save File Backup Script --- [START] load failed. Retrying...
+		echo ===========================
+        echo load failed. Retrying...
+		echo ===========================
+        timeout /t 5 > nul
+        goto DownloadRetryLoop
+    ) else (
+	    color 04
+	    title Monster Hunter Wilds : Save File Backup Script --- [START] Failed to load Scripts after 3 attempts.
+		echo ===========================
+        echo Failed to load Scripts after 3 attempts.
+		echo ===========================
+        timeout /t 5 > nul
+        endlocal
+        exit /b 1
+    )
+)
+endlocal
+exit /b 0
+:VerifyHash
+setlocal enabledelayedexpansion
+set "FilePath=%~1"
+set "Expected=%~2"
+echo.
+title Monster Hunter Wilds : Save File Backup Script --- [START] Verifying script hash...
+color 03
+echo =========================== Verifying script hash... ===========================
+timeout /t 5 > nul
+for /f "tokens=*" %%A in ('certutil -hashfile "%FilePath%" SHA256 ^| findstr /i /r "^[0-9a-f]"') do (
+    set "Actual=%%A"
+    set "Actual=!Actual: =!"
+    goto GotHash
+)
+:GotHash
+echo ===========================                         ===========================
+echo Expected Hash: %Expected%
+echo Actual   Hash: !Actual!
+echo ===========================                         ===========================
+if /I "!Actual!" neq "%Expected%" (
+    title Monster Hunter Wilds : Save File Backup Script --- [START] Script Hash verification failed.
+	color 04
+    echo Script Hash verification failed.
+    timeout /t 5 > nul
+    endlocal
+    exit /b 1
+)
+title Monster Hunter Wilds : Save File Backup Script --- [START] Script Hash verified.
+color 0A
+echo =========================== Script Hash verified. ===========================
+timeout /t 5 > nul
+cls
+endlocal
+exit /b 0
