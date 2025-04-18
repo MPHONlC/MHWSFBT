6ace3dc7f5378c62e6a0ab3094f79eabc28e2ac6
MPHONlC <missaphonic@gmail.com>
2025-04-18 08:33:21 +0800

    Update Monitor.bat

--- Monitor.bat
+++ Monitor.bat
@@ -5,6 +5,7 @@ title Monster Hunter Wilds : Save File Backup Script --- Monitoring Scripts
 for /f "tokens=3" %%A in ('reg query "HKCU\Console" /v QuickEdit') do reg add "HKCU\Console" /v QuickEdit /t REG_DWORD /d 0 /f >nul
 echo Monster Hunter Wilds : Save File Backup Script --- [INFO] Waiting for other proccesses to finish loading...
 timeout /t 60 > nul
+cls
 set "SFBScript=SFB.bat"
 set "BackupToolScript=MHWSaveFileBackupTool.bat"
 set "StartScript=Start.bat"
